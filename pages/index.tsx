cfcccbda6d80d421547479d8c7111e80f663add6
nolbuzanis <nolanbuzanis@gmail.com>
2021-05-25 23:02:36 -0400

    Using link to navigate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import Link from 'next/link';
+
 const HomePage = (): JSX.Element => {
   return (
     <div>
-      <h1>hello next world</h1>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/portfolio'>Portfolio</Link>
+        </li>
+        <li>
+          <Link href='/clients'>Clients</Link>
+        </li>
+        <li>
+          <Link href='/about'>About</Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
