4124bb3305427ffe5f2f4d75a2b94efc7ed81625
nolbuzanis <nolanbuzanis@gmail.com>
2021-05-25 23:08:14 -0400

    creating dynamic navigation

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,5 +1,24 @@
-const ClientsPage = (): JSX.Element => {
-  return <div>Clients Page</div>
-}
+import Link from 'next/link';
 
-export default ClientsPage
+const ClientsPage = (): JSX.Element => {
+  const clients = [
+    { id: 'nol', name: 'Nolan' },
+    { id: 'jer', name: 'Jeremy' },
+    { id: 'em', name: 'Emily' },
+  ];
+
+  return (
+    <div>
+      <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ClientsPage;
