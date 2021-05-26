6f914cfc18a434579fc27ceeba5519b8452dcf0a
nolbuzanis <nolanbuzanis@gmail.com>
2021-05-25 23:10:45 -0400

    alternative for using link

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -13,7 +13,14 @@ const ClientsPage = (): JSX.Element => {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            <Link
+              href={{
+                pathname: '/clients/[id]',
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
